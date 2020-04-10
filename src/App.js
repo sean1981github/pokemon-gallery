7604992e25ead71c2aa0f7ea489fd84cafd78af0
sean1981github <sean1981@gmail.com>
2020-04-10 15:13:36 +0800

    my first commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,61 @@ import React from "react";
 import "./App.css";
 import pokemonData from "./pokemon/pokemon";
 
-const bulbasaur = pokemonData[0];
+const pokemonSelected = pokemonData[25];
+
+function elementBackgroundColorSelector(element) {
+  const colorMap = [
+    { type: "Grass", color: "seagreen" },
+    { type: "Poison", color: "thistle" },
+    { type: "Fire", color: "orangered" },
+    { type: "Flying", color: "lightblue" },
+    { type: "Water", color: "turquoise" },
+    { type: "Bug", color: "greenyellow" },
+    { type: "Normal", color: "white" },
+    { type: "Electric", color: "yellow" },
+    { type: "Ground", color: "darkgoldenrod" },
+    { type: "Fairy", color: "pink" },
+  ];
+
+  return colorMap
+    .filter((typeOfElement) => typeOfElement.type === element)
+    .map((colorType) => {
+      return (
+        <div class="element" style={{ "background-color": colorType.color }}>
+          {element}
+        </div>
+      );
+    });
+}
 
 function PokemonCard({ pokemon }) {
   const { id, name, type, base } = pokemon;
+
   return (
-    <div>
-      <div>{name.english}</div>
+    <div class="pokecard">
+      <div class="id">
+        <img
+          src={
+            process.env.PUBLIC_URL + "pokemonImage/" + id.toString() + ".png"
+          }
+          alt={name.english}
+        />
+      </div>
+      <div class="name">{name.english}</div>
+
+      <div class="element-area">
+        {type.map((element) => {
+          return elementBackgroundColorSelector(element);
+        })}
+      </div>
+      <div class="base-area">
+        <div class="hp">HP:{base.HP}</div>
+        <div class="attack">Attack:{base.Attack}</div>
+        <div class="defence">Defence:{base.Defence}</div>
+        <div class="sp-attack">SP-Attack:{base.SpAttack}</div>
+        <div class="sp-defence">SP-Defence:{base.SpDefence}</div>
+        <div class="speed">Speed:{base.Speed}</div>
+      </div>
     </div>
   );
 }
@@ -16,7 +64,7 @@ function PokemonCard({ pokemon }) {
 function App() {
   return (
     <div className="App">
-      <PokemonCard pokemon={bulbasaur} />
+      <PokemonCard pokemon={pokemonSelected} />
     </div>
   );
 }
